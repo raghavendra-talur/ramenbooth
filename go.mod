53faaa602bef6b5f422e36aa05846504707d6344
Raghavendra Talur <raghavendra.talur@gmail.com>
2024-05-30 16:21:00 -0400

    more
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- go.mod
+++ go.mod
@@ -5,12 +5,18 @@ go 1.22.0
 require (
 	github.com/charmbracelet/bubbletea v0.26.3
 	github.com/charmbracelet/lipgloss v0.11.0
-	k8s.io/apimachinery v0.30.1
-	k8s.io/client-go v0.30.1
+	github.com/open-cluster-management/api v0.0.0-20210527013639-a6845f2ebcb1
+	github.com/ramendr/ramen/api v0.0.0-20240117171503-e11c56eac24d
+	go.uber.org/zap v1.25.0
+	k8s.io/api v0.30.1
+	k8s.io/client-go v12.0.0+incompatible
+	sigs.k8s.io/controller-runtime v0.16.3
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/charmbracelet/x/ansi v0.1.1 // indirect
 	github.com/charmbracelet/x/input v0.1.0 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
@@ -18,16 +24,21 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/zapr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -35,15 +46,24 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stolostron/multicloud-operators-placementrule v1.2.4-1-20220311-8eedb3f.0.20230828200208-cd3c119a7fa0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20220909182711-5c715a9e8561 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/oauth2 v0.10.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
@@ -51,16 +71,24 @@ require (
 	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.30.1 // indirect
+	k8s.io/apiextensions-apiserver v0.28.3 // indirect
+	k8s.io/apimachinery v0.30.1 // indirect
+	k8s.io/component-base v0.28.3 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
+)
+
+replace (
+	github.com/open-cluster-management-io/api => open-cluster-management.io/api v0.10.0
+	k8s.io/client-go v12.0.0+incompatible => k8s.io/client-go v0.29.0
 )
--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	ocmworkv1 "github.com/open-cluster-management/api/work/v1"
+	ramen "github.com/ramendr/ramen/api/v1alpha1"
+	"go.uber.org/zap/zapcore"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var (
@@ -33,8 +39,19 @@ type clusterInfo struct {
 	status         string
 	context        string
 	namespaces     []string
+	DRPCs          []string
 	hub            bool
 	managedcluster bool
+}
+
+func newClusterInfo(name, context string, hub, managedcluster bool) clusterInfo {
+	return clusterInfo{
+		name:           name,
+		status:         "Unknown",
+		context:        context,
+		hub:            hub,
+		managedcluster: managedcluster,
+	}
 }
 
 type model struct {
@@ -46,17 +63,20 @@ type model struct {
 }
 
 func initialModel() model {
-	return model{
-		clusters: []clusterInfo{
-			{"Hub", "Unknown", hub, []string{}, true, false},
-			{"DR1", "Unknown", dr1, []string{}, false, true},
-			{"DR2", "Unknown", dr2, []string{}, false, true},
-		},
-	}
+	var m model
+	var clusters []clusterInfo
+
+	clusters = append(clusters, newClusterInfo("Hub", hub, true, false))
+	clusters = append(clusters, newClusterInfo("DR1", dr1, false, true))
+	clusters = append(clusters, newClusterInfo("DR2", dr2, false, true))
+
+	m.clusters = clusters
+
+	return m
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.Batch(fetchClustersData(&m.clusters), tickCmd())
+	return tea.Batch(updateClustersData(&m.clusters), tickCmd())
 }
 
 func tickCmd() tea.Cmd {
@@ -65,32 +85,33 @@ func tickCmd() tea.Cmd {
 	})
 }
 
-func fetchClusterClient(kubeconfig string) *kubernetes.Clientset {
+func fetchClusterClient(kubeconfig string) client.Client {
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		return nil
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	kclient, err := client.New(config, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
 		return nil
 	}
 
-	return clientset
+	return kclient
 }
 
 func updateClusterData(c *clusterInfo) {
-	client := fetchClusterClient(c.context)
-	if client == nil {
+	kclient := fetchClusterClient(c.context)
+	if kclient == nil {
 		c.status = "Error"
 		c.namespaces = []string{}
 	}
 
-	c.status = getClusterStatus(client)
-	c.namespaces = getRamenNamespaces(client)
+	c.status = getClusterStatus(c, kclient)
+	c.namespaces = getRamenNamespaces(c, kclient)
+	c.DRPCs = getDRPCs(c, kclient)
 }
 
-func fetchClustersData(clusters *[]clusterInfo) tea.Cmd {
+func updateClustersData(clusters *[]clusterInfo) tea.Cmd {
 	return func() tea.Msg {
 		for i := range *clusters {
 			updateClusterData(&(*clusters)[i])
@@ -99,35 +120,57 @@ func fetchClustersData(clusters *[]clusterInfo) tea.Cmd {
 	}
 }
 
-func filterRamenNamespaces(namespaces *v1.NamespaceList) []string {
+func filterRamenNamespaces(namespaces *corev1.NamespaceList) []string {
 	var filtered []string
 	for _, ns := range namespaces.Items {
 		if ns.Name == "ramen-system" ||
 			ns.Name == "ramen-ops" ||
 			ns.Name == "openshift-operators" ||
-			ns.Name == "openshift-dr-system" {
+			ns.Name == "openshift-dr-system" ||
+			ns.Name == "openshift-dr-ops" {
 			filtered = append(filtered, ns.Name)
 		}
 	}
 	return filtered
 }
 
-func getNamespaces(clientset *kubernetes.Clientset) *v1.NamespaceList {
-	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+func getNamespaces(kclient client.Client) *corev1.NamespaceList {
+	namespaces := &corev1.NamespaceList{}
+	err := kclient.List(context.Background(), namespaces, &client.ListOptions{})
 	if err != nil {
-		return &v1.NamespaceList{}
+		return &corev1.NamespaceList{}
 	}
 
 	return namespaces
 }
 
-func getRamenNamespaces(clientset *kubernetes.Clientset) []string {
-	ns := getNamespaces(clientset)
+func getRamenNamespaces(c *clusterInfo, kclient client.Client) []string {
+	ns := getNamespaces(kclient)
 	return filterRamenNamespaces(ns)
 }
 
-func getClusterStatus(clientset *kubernetes.Clientset) string {
-	_, err := clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+func getDRPCs(c *clusterInfo, kclient client.Client) []string {
+	if !c.hub {
+		return []string{}
+	}
+
+	drpcs := &ramen.DRPlacementControlList{}
+	err := kclient.List(context.Background(), drpcs, &client.ListOptions{})
+	if err != nil {
+		return []string{}
+	}
+
+	var drpcNames []string
+	for _, drpc := range drpcs.Items {
+		drpcNames = append(drpcNames, drpc.Name)
+	}
+
+	return drpcNames
+}
+
+func getClusterStatus(c *clusterInfo, kclient client.Client) string {
+	nodes := &corev1.NodeList{}
+	err := kclient.List(context.Background(), nodes, &client.ListOptions{})
 	if err != nil {
 		return "Error"
 	}
@@ -160,16 +203,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		}
 	case time.Time:
-		return m, tea.Batch(fetchClustersData(&m.clusters), tickCmd())
+		return m, tea.Batch(updateClustersData(&m.clusters), tickCmd())
 	}
 	return m, nil
 }
 
 func getHubStyle(cluster *clusterInfo, width, height int) string {
 	var msg []string
+
 	msg = append(msg, fmt.Sprintf("%s\n\n", cluster.name))
 	msg = append(msg, fmt.Sprintf("Status: %s\n", cluster.status))
-	msg = append(msg, fmt.Sprintf("Namespaces: %s\n", cluster.namespaces))
+	msg = append(msg, fmt.Sprintf("Namespaces: %s\n", strings.Join(cluster.namespaces, ",")))
+	msg = append(msg, fmt.Sprintf("DRPCs: %s\n", strings.Join(cluster.DRPCs, ",")))
 	style := lipgloss.NewStyle().
 		Border(lipgloss.NormalBorder(), true).
 		Width(width).
@@ -180,7 +225,7 @@ func getHubStyle(cluster *clusterInfo, width, height int) string {
 	return style.Render()
 }
 
-func getManagedClusterStyle(cluster *clusterInfo, width, height int, align string) string {
+func getManagedClusterStyle(cluster *clusterInfo, width, height int) string {
 	var msg []string
 	msg = append(msg, fmt.Sprintf("%s\n\n", cluster.name))
 	msg = append(msg, fmt.Sprintf("Status: %s\n", cluster.status))
@@ -201,8 +246,8 @@ func (m model) View() string {
 	var dr2Style string
 
 	hubStyle = getHubStyle(&m.clusters[0], m.width, m.height/3)
-	dr1Style = getManagedClusterStyle(&m.clusters[1], m.width/2, m.height/2, "left")
-	dr2Style = getManagedClusterStyle(&m.clusters[2], m.width/2, m.height/2, "right")
+	dr1Style = getManagedClusterStyle(&m.clusters[1], m.width/2, m.height/2)
+	dr2Style = getManagedClusterStyle(&m.clusters[2], m.width/2, m.height/2)
 
 	// Render the final view with the new layout
 	s := lipgloss.JoinVertical(
@@ -217,7 +262,62 @@ func (m model) View() string {
 	return s
 }
 
+func addSchemes() {
+	err := ocmworkv1.AddToScheme(scheme.Scheme)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = ramen.AddToScheme(scheme.Scheme)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// err = ocmclv1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = plrv1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = viewv1beta1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = cpcv1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = gppv1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = Recipe.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = volrep.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = volsyncv1alpha1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = snapv1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+	// Expect(velero.AddToScheme(scheme.Scheme)).To(Succeed())
+
+	// err = clrapiv1beta1.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+
+	// err = argocdv1alpha1hack.AddToScheme(scheme.Scheme)
+	// Expect(err).NotTo(HaveOccurred())
+}
+
 func main() {
+	testLogger := zap.New(zap.UseFlagOptions(&zap.Options{
+		Development: true,
+		DestWriter:  os.Stdout,
+		TimeEncoder: zapcore.ISO8601TimeEncoder,
+	}))
+	logf.SetLogger(testLogger)
+	addSchemes()
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
